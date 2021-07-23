57ae49adce3c58e83e88246c57e5de63ae6912ac
huzaifa0786 <huznoone7@gmail.com>
2021-07-23 20:14:40 +0530

    commit

--- data.py
+++ data.py
@@ -22,9 +22,10 @@ driver = webdriver.Chrome(r"C:\Users\MSI\.wdm\drivers\chromedriver\win32\91.0.44
 driver.get(url)
 time.sleep(10)
 soup = BeautifulSoup(driver.page_source,"html5lib")
-for  param in range(1,70):
+def norm(fetch_data)
+for  param in range(1,40):
     #data=os.Path.join(r"C:\Users\MSI\.wdm\drivers\chromedriver\win32\91.0.4472.101\chromedriver.exe")
-    url="https://www.naukri.com/python-jobs"
+    url="https://www.naukri.com/"+s
     # page_len=(len(url))
     # page = requests.get(url)
     # page.otext
@@ -66,15 +67,54 @@ for  param in range(1,70):
         'Title','Company','Ratings','Reviews','URL'
         df=df.append({'URL' : url,'Title' : title,"Company": Com.text.upper() ,'Ratings' : Rating,'Reviews' : Reviews},ignore_index=True)
 df.head()
-df.to_csv("Naukri.csv",index=False)
+
 def norm(x):##This function to normalize teh title.
     y=x.split(" ")
-    if("data" in y )and ("analyst" in y):
+    if("DATA" in y )and ("ANALYST" in y):
     
-        return("Data Analyst")
+        return("DATA ANALYST")
+    elif("SENIOR" in y )and ("PYTHON" in y):
+        return("SENIOR PYTHON ENGINEER")
+    elif("DATA" in y )and ("ENGINEER" in y):
+        return("DATA ENGINEER")
+    elif("PERMOFORMANCE" in y )and ("TEST" in y):
+        return("PERFORMANCE TEST ENGINEER")
+    elif("BIG" in y )and ("BIG" in y):
+        return("BIG DATA ENGINEER")
+    elif("SENIOR" in y ) or ('SR' in y)and ("ANALYST" in y):
+        return("SENIOR DATA ANALYST")
+    elif("PYTHON" in y) and ("DESIGNER" in y):
+        return("PYTHON DESIGNER")
+    else:
+        return(x)
+    
+    
+for i in range(df.shape[0]):
+    
+    df["Title"][i]=norm(df["Title"][i])
+df.to_csv("Naukri1.csv",index=False)
+
+def norm(x):##This function to normalize teh title.
+    y=x.split(" ")
+    if("DATA" in y )and ("ANALYST" in y):
+    
+        return("DATA ANALYST")
+    if("SENIOR" in y )and ("PYTHON" in y):
+        return("SENIOR PYTHON ENGINEER")
+    if("DATA" in y )and ("ENGINEER" in y):
+        return("DATA ENGINEER")
+    if("PERMOFORMANCE" in y )and ("TEST" in y):
+        return("PERFORMANCE TEST ENGINEER")
+    if("BIG" in y )and ("BIG" in y):
+        return("BIG DATA ENGINEER")
+    if("SENIOR" in y ) or ('SR' in y)and ("ANALYST" in y):
+        return("SENIOR DATA ANALYST")
+    if("PYTHON" in y) and ("DESIGNER" in y):
+        return("PYTHON DESIGNER")
     
     
 for i in range(df.shape[0]):
     df["Title"][i]=norm(df["Title"][i])
+    
 
-df = pd.DataFrame(columns=['Title','Company','Ratings','Reviews','URL'])
+
